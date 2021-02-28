2b642cd7dfdd429bcadeee731d1178ef26cf0d60
Roman <roman.kukla@mail.ru>
2021-02-28 16:20:34 +0300

    completed homework

--- HomeWork 1/script.js
+++ HomeWork 1/script.js
@@ -11,20 +11,30 @@ function fib(n) {
     }
 }
 
+console.log(fib(8));
 console.log(fib(610));
 
 //2
 
 function flatArray(arr) {
-    return arr.reduce((newArr, value) => newArr.concat(Array.isArray(value) ? flatArray(value) : value), []);
+    return arr.reduce((newArr, value) => newArr.concat(Array.isArray(value) ? flatArray(value) : value), []).sort((a, b) => {
+        return a - b;
+    });
 }
+
 function difference(arr1, arr2) {
-    return flatArray(arr1).concat(flatArray(arr2)).filter(x => {
-        return !arr1.includes(x) || !arr2.includes(x)
+    return flatArray(arr1).concat(flatArray(arr2)).filter((x, index, array)=> {
+        return array.indexOf(x) === index;
+    }).sort((a, b) => {
+        return a - b;
     })
 }
 
-console.log(difference([1, 2, 3], [3, 4, 5]))
+console.log(difference([1, 2, 3], [100, 2, 1, 10]));
+console.log(difference([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]));
+console.log(difference([1, 2, 3], [100, 2, 1, 10]));
+
+console.log(difference([1, 11, 9, 4, 4, 4, [9], [6], 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]));
 
 //3
 function caseInsensitiveSearch(str, word) {
@@ -45,6 +55,8 @@ function copyObject(obj) {
 }
 
 console.log(copyObject({a: 1, b: 2}));
+console.log(copyObject({name: 'Sasha', age: 99}));
+console.log(copyObject({title: 'New book', type: 'Horror'}));
 
 //5
 function convertObjectToArray(obj) {
@@ -56,15 +68,25 @@ function convertObjectToArray(obj) {
 }
 
 console.log(convertObjectToArray({a: 'aa',b: 2, c: 3}));
+console.log(convertObjectToArray({name: 'Sasha', age: 99}));
+console.log(convertObjectToArray({title: 'New book', type: 'Horror'}));
 
 //6
 function uncamelize(str, sep = ' ') {
-    return str.replace(/[A-Z]/g, (l, index) => {
-        return index !== 0 ? sep + l.toLowerCase() : l.toLowerCase();
-    });
+    return str.split('').map((x, index) => {
+        if (x.charCodeAt(0) >= 65 && x.charCodeAt(0) <= 90) {
+            if (index === 0)
+                return x.toLowerCase();
+            return `${sep}${x.toLowerCase()}`;
+        } else {
+            return x;
+        }
+    }).join('');
 }
 
-console.log(uncamelize("HelloWord", '-'))
+console.log(uncamelize('helloWorld'));
+console.log(uncamelize('helloWorld','-'));
+console.log(uncamelize('HelloWorld','_'));
 
 //7
 function countSubstrInStr(str, subtr) {
@@ -72,11 +94,14 @@ function countSubstrInStr(str, subtr) {
 }
 
 console.log(countSubstrInStr('carcarcar', 'ca'));
+console.log(countSubstrInStr('qweqweqwqwwqe', 'qwe'));
 
 
 //8 
 function flatArray(arr) {
-    return arr.reduce((newArr, value) => newArr.concat(Array.isArray(value) ? flatArray(value) : value), []);
+    return arr.reduce((newArr, value) => newArr.concat(Array.isArray(value) ? flatArray(value) : value), []).sort((a, b) => {
+        return a - b;
+    });
 }
 
 console.log(flatArray([1, 2, 3, [4], 5, [[6]], [[[[6]]]], [1, 2, 3, [[7]]]]));
@@ -86,16 +111,22 @@ console.log(flatArray([1, 2, 1000, 300, [400, [3, 10, [11, 12]], [1, 2, [3, 4]],
 function deleteNullOrUndefinedFromArray(arr, callback) {
     setTimeout(() => {
         callback(
+            null,
             arr.filter(x => {
                 return x != null || x != undefined;
             })
-        )
+        );
     }, 5000);
 }
 
-deleteNullOrUndefinedFromArray([1, 2, 3, null ,undefined, null, 1, 1, 1, undefined], arr => {
-    console.log(arr)
-})
+deleteNullOrUndefinedFromArray([1, 2, 3, null ,undefined, null, 1, 1, 1, undefined], (err, arr) => {
+    if (err){
+        console.log(err);
+    }
+    else {
+        console.log(arr);
+    }
+});
 
 //10
 function returnPromise(){
@@ -106,31 +137,78 @@ function returnPromise(){
     })
 }
 
-returnPromise().then((result) => {
+returnPromise().then(result => {
     console.log(`Result: ${result}`);
 }, error => {
-    console.log(`Error: ${error}`)
+    console.log(`Error: ${error}`);
 });
 
 //11
-function arrayPrimises(arr) {
-    for (let i in arr) {
-
+async function runPromisesInSeries(tasks) {
+    for (let el of tasks) {
+        console.log(await el);
     }
 }
 
-//12
+runPromisesInSeries([
+    new Promise((resolve, reject) => {setTimeout(() => {resolve("Success 1")}, 2000)}),
+    new Promise((resolve, reject) => {setTimeout(() => {resolve("Success 2")}, 5100)}),
+    new Promise((resolve, reject) => {setTimeout(() => {resolve("Success 3")}, 1800)}),
+    new Promise((resolve, reject) => {setTimeout(() => {resolve("Success 4")}, 1100)})
+]);
 
-async function parallelRequests() {
-    await Promise.all([
-        fetch('http://geocode.xyz/Minsk?json=1&auth=869122208963241887711x53952').then(res =>
-            console.log(res.json())
-        ),
-        fetch('http://geocode.xyz/Madrid?json=1&auth=869122208963241887711x53952').then(res => 
-            console.log(res.json())
-        ),
-        fetch('http://geocode.xyz/Rome?json=1&auth=869122208963241887711x53952').then(res => 
-            console.log(res.json())
-        )
-    ])
-} 
+//12
+function parallelRequests() {
+    document.getElementById('task1').innerHTML = '';
+    Promise.all([
+        fetch('http://geocode.xyz/Minsk?json=1&auth=869122208963241887711x53952').then(res => res.json()),
+        fetch('http://geocode.xyz/Madrid?json=1&auth=869122208963241887711x53952').then(res => res.json()),
+        fetch('http://geocode.xyz/Rome?json=1&auth=869122208963241887711x53952').then(res => res.json()),
+    ]).then(responces => {
+        responces.forEach(x => {
+            document.getElementById('task1').innerHTML += `${x.standard.city} - ${x.standard.countryname} <br>`;
+        });
+    }, error => {
+        new Error(error);
+    });
+}
+
+function promiseRace() {
+    document.getElementById('task2').innerHTML = '';
+    Promise.race([
+        fetch('http://geocode.xyz/Paris?json=1&auth=869122208963241887711x53952').then(res => res.json()),
+        fetch('http://geocode.xyz/Nice?json=1&auth=869122208963241887711x53952').then(res => res.json()),
+    ]).then(res => {
+        document.getElementById('task2').innerHTML = `City: ${res.standard.city}, Country: ${res.standard.countryname}`
+    }, error => {
+        new Error(error);
+    })
+}
+
+function getCities(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Kyiv Moscow Warszawa Vilnius Riga");
+        }, 3000);
+    })
+}
+
+function promiseResolves() {
+    document.getElementById('task3').innerHTML = '';
+    getCities().then(result => {
+        let links = result.split(' ').map(x => {
+            return `http://geocode.xyz/${x}?json=1&auth=869122208963241887711x53952`;
+        })
+
+        Promise.all(links.map(x => fetch(x))).then(responses =>
+            Promise.all(responses.map(res => res.json()))
+        ).then(responces => {
+            responces.forEach(x => {
+                document.getElementById('task3').innerHTML += `City: ${x.standard.city}, County: ${x.standard.countryname} <br>`
+            }), 
+            error => {
+                new Error(error);
+            }
+        })
+    })
+}
